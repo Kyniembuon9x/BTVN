4a4f8e16d6b8403997f0b5e18e5f9bbed213bb67
Tung Lam <lam.vuong105@gmail.com>
2025-07-14 08:50:02 +0700

    BTVN12

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-const input = document.getElementById("task-input");
-const addBtn = document.getElementById("add-btn");
-const taskList = document.getElementById("task-list");
 
-addBtn.addEventListener("click", () => {
-  const taskName = input.value.trim();
-  if (taskName === "") {
-    alert("Vui lòng nhập tên công việc!");
-    return;
-  }
+let students = [];
 
-  const li = document.createElement("li");
-  li.textContent = taskName;
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Xoá";
-  deleteBtn.classList.add("delete-btn");
-  deleteBtn.addEventListener("click", () => {
-    li.remove();
-  });
+students.push("An", "Bình", "Chi");
 
-  li.appendChild(deleteBtn);
-  taskList.appendChild(li);
 
-  input.value = "";
-});
+const indexBinh = students.indexOf("Bình");
+if (indexBinh !== -1) {
+    students.splice(indexBinh, 1);
+}
+
+students.unshift("Dũng");
+
+console.log("Danh sách học sinh cuối cùng:", students);
